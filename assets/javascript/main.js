d824b9949bd571e7fa88ca837e0c9917adc78856
Kayle7777 <jessewebb2@gmail.com>
2018-07-30 18:18:23 -0500

    Updates

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,15 @@
-$("#headerLinks .btn").click(function() {
+let docs = {
+  "Home": `HomeText`,
+  "Middle": `MiddleText`,
+  "Right": `RightText`,
+  "Other Stuff": `TestText`
+}
+
+$("#headerLinks .btn, .dropdown-menu .dropdown-item").click(function(event) {
+  event.preventDefault();
   $("#headerLinks").children().removeClass("active");
-  console.log("test");
+  let text = $(this).text();
+  $(".card-body").html(`
+    <p>${docs[text]}</p>
+    `)
 })
