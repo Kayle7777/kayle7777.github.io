78cb29857ee26fbc6b57d59018cc4026f1464ce3
Kayle7777 <jessewebb2@gmail.com>
2018-12-06 12:24:00 -0600

    mern stack portfolio start

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default {
+    // Gets all things
+    getStuff: function() {
+        return axios.get('/api/example');
+    },
+    // Gets the thing with the given id
+    getStuff: function(id) {
+        return axios.get('/api/example/' + id);
+    },
+    // Deletes stuff with the given id
+    deleteStuff: function(id) {
+        return axios.delete('/api/example/' + id);
+    },
+    // Saves stuff to the database
+    saveStuff: function(stuffData) {
+        return axios.post('/api/example', stuffData);
+    },
+};
