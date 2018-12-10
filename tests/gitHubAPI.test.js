ba0baa5b06132e9783d10e22ca44189ea40ba8fe
Kayle7777 <jessewebb2@gmail.com>
2018-12-10 06:27:03 -0600

    Fixed express API tests. Some solid proof there that the graphql version is way better to use, time on v4 vs. v3 is way less.

--- tests/gitHubAPI.test.js
+++ tests/gitHubAPI.test.js
@@ -3,22 +3,34 @@ const server = require('../server');
 
 jest.setTimeout(30000);
 
-test('github v3 repos', async () => {
-    let gotData = await request(server)
-        .get('/api/gitHub/')
-        .expect(200);
-    let failCount = 0;
-    // for (let x of gotData.body.repos) {
-    //     if (!x.readme_url) failCount++;
-    // }
-    // console.log(gotData.body);
-    // expect(gotData.body.repos.length).toBeGreaterThan(1);
-    // expect(failCount).toBeLessThan(15);
+test('github v3 standard API repos', async () => {
+    try {
+        let gotData = await request(server)
+            .get('/api/gitHub/')
+            .expect(200);
+        let failCount = 0;
+        for (let x of gotData.body.repos) {
+            if (!x.readme_url) failCount++;
+        }
+        expect(gotData.body.repos.length).toBeGreaterThan(1);
+        expect(failCount).toBeLessThan(15);
+    } catch (err) {
+        throw err;
+    }
 });
 
-test('github v4 repos', async () => {
-    let gotData = await request(server)
-        .post('/api/gitHub/graphql')
-        .expect(200);
-    // console.log(gotData.body.data.viewer.repositories);
+test('github v4 graphql API repos', async () => {
+    try {
+        let gotData = await request(server)
+            .post('/api/gitHub/graphql')
+            .expect(200);
+        let failCount = 0;
+        // gotData.body.data.viewer.repositories.nodes;
+        for (let x of gotData.body.data.viewer.repositories.nodes) {
+            if (!x.readme) failCount++;
+        }
+        expect(failCount).toBeLessThan(15);
+    } catch (err) {
+        throw err;
+    }
 });
