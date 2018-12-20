ac5bf3e1e16a8ec186486069b4bf40421f74ac73
Kayle7777 <jessewebb2@gmail.com>
2018-12-19 23:38:30 -0600

    I think that was it...

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const path = require('path');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
