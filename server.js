42a13c578f850cced227c3e4852f6d54aa5cecae
Kayle7777 <jessewebb2@gmail.com>
2018-12-19 23:24:39 -0600

    ci cd is hard

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 const app = express();
 const PORT = 3001;
 const bodyParser = require('body-parser');
@@ -12,12 +13,8 @@ app.use(cors());
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 }
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
 
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
