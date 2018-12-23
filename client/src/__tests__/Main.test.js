e36424dd6c77f16248bc554aa2f197d655baeb33
Kayle7777 <jessewebb2@gmail.com>
2018-12-22 23:47:58 -0600

    Update test

--- client/src/__tests__/Main.test.js
+++ client/src/__tests__/Main.test.js
@@ -3,17 +3,17 @@ import { render } from 'react-testing-library';
 import request from 'supertest';
 import app from '../../../server';
 import Main from '../pages/Main';
-import dotenv from 'dotenv';
-dotenv.config();
 jest.setTimeout(15000);
 
-test('render Main with data test', async () => {
-    try {
-        const apiResponse = await request(app).post('/api/gitHub/graphql');
-        const { getByText } = render(<Main testData={apiResponse.body} />);
-        const header = getByText('Jesse Webb');
-        expect(header.textContent).toBe('Jesse Webb');
-    } catch (e) {
-        throw new Error(e);
-    }
+describe('testing Main.js', () => {
+    test('render Main with real data', async () => {
+        try {
+            const apiResponse = await request(app).post('/api/gitHub/graphql');
+            const { getByText } = render(<Main testData={apiResponse.body} />);
+            const header = getByText('Jesse Webb');
+            expect(header.textContent).toBe('Jesse Webb');
+        } catch (e) {
+            throw new Error(e);
+        }
+    });
 });
